f9eed36be3009273430d970ba2e7e5c1f6ce2355
servomouse <servomouse@protonmail.com>
2025-05-14 19:50:19 -0600

    Add user interactions

--- emulator.py
+++ emulator.py
@@ -1,6 +1,36 @@
 import threading
 import time
+import sys
 from queue import Queue
+from pynput import keyboard # pip install pynput
+
+
+class UserInputGetter:
+    def __init__(self):
+        self.input_queue = Queue()
+        self.part_input = ""
+        self.command = None
+        self.listener = keyboard.Listener(on_press=self.key_press)
+        self.listener.start()
+
+    def get_part_input(self):
+        return self.part_input
+
+    def get_command(self):
+        cmd = self.command
+        self.command = None
+        return cmd
+
+    def key_press(self, key):
+        if key == keyboard.Key.enter:
+            self.command = self.part_input
+            self.part_input = ""
+        elif key == keyboard.Key.backspace:
+            self.part_input = self.part_input[:-1]
+        elif key == keyboard.Key.space:
+            self.part_input += " "
+        elif hasattr(key, 'char'):
+            self.part_input += key.char
 
 
 def print_to_file(string):
@@ -8,39 +38,59 @@ def print_to_file(string):
         f.write(string + "\n")
 
 
-def emulator_thread(command_queue):
+def print_line(line, part_cmd):
+    print(f"\033[2K\r{line}")
+    print(f"Enter command: {part_cmd}", end = "")
+    sys.stdout.flush()
+
+
+def emulator_thread(command_queue, ui_getter):
     inner_state = 'running'
     while True:
+        part_cmd = ui_getter.get_part_input()
         if not command_queue.empty():
             cmd = command_queue.get()
-            print_to_file(f"User cmd from thread: {cmd}")
+            print_line(f"User cmd from thread: {cmd}", part_cmd)
             if cmd == 'run':
                 inner_state = 'running'
             elif cmd == 'print value':
-                print_to_file("Here is the value!")
+                print_line("Here is the value!", part_cmd)
             elif cmd == 'stop':
                 inner_state = 'stopped'
-                print_to_file("The thread is stopped")
+                print_line("The thread is stopped", part_cmd)
             elif cmd == 'quit':
                 return
             command_queue.task_done()
         elif inner_state == 'running':
-            print_to_file("Hello from the thread!")
+            print_line("Hello from the thread!", part_cmd)
             time.sleep(1)
+
 
 if __name__ == "__main__":
     print("Starting the thread")
+    # print(f"{dir(keyboard.Key) = }")
+    # sys.exit(0)
 
     command_queue = Queue()
+    ui_getter = UserInputGetter()
 
-    t1 = threading.Thread(target=emulator_thread, args=(command_queue,))
+    t1 = threading.Thread(target=emulator_thread, args=(command_queue, ui_getter))
     t1.setDaemon(True)
     t1.start()
 
+    last_update = time.time()
+    last_input = ""
     while True:
-        user_command = input("Enter a command: ")
-        print(f"User command: \"{user_command}\"")
-        command_queue.put(user_command)
-        if user_command == 'quit':
-            break
-        print(f"Waiting for user command...")
+        part_input = ui_getter.get_part_input()
+        command = ui_getter.get_command()
+
+        # if time.time()-last_update > 1:
+        #     print_line(f"Counter = {counter}", part_input)
+        #     counter += 1
+        #     last_update = time.time()
+
+        if command is not None:
+            print_line(f"User input: {command}", part_input)
+            command_queue.put(command)
+            if command == "quit":
+                break
