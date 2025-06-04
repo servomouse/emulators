acd7aa73c5b996a350527223e4cb18e5dcc883b5
servomouse <servomouse@protonmail.com>
2025-06-04 17:27:15 -0600

    Make test fwork as a test

--- devices/tests/test_brainfuck/test_brainfuck_cpu.c
+++ devices/tests/test_brainfuck/test_brainfuck_cpu.c
@@ -10,7 +10,7 @@ char p_memory[] = "++++[->,++++.----<]";
 uint8_t d_memory[128] = {0};
 
 uint8_t mem_read(uint32_t addr) {
-    uint8_t is_p_mem = 0, val;
+    uint8_t val;
     if(addr >= PROGRAM_OFFSET) {
         addr -= PROGRAM_OFFSET;
         if(addr >= sizeof(p_memory)) {
@@ -25,12 +25,10 @@ uint8_t mem_read(uint32_t addr) {
         }
         val = d_memory[addr];
     }
-    // printf("Reading memory addr = %d, val = %d\n", addr, val);
     return val;
 }
 
 void mem_write(uint32_t addr, uint8_t val) {
-    // printf("Writing memory addr = %d, val = %d\n", addr, val);
     if(addr >= MEMORY_SIZE) {
         printf("Error: Attempting to write to outside the d_memory! Address = %d\n", addr);
         exit(EXIT_FAILURE);
@@ -38,26 +36,28 @@ void mem_write(uint32_t addr, uint8_t val) {
     d_memory[addr] = val;
 }
 
-uint8_t io_inputs[] = {5, 7, 123, 0};
+#define NUM_INPUTS 4
+
+uint8_t io_inputs[NUM_INPUTS] = {5, 7, 123, 0};
 uint8_t io_in_ptr = 0;
-uint8_t io_outputs[4] = {0};
+uint8_t io_outputs[NUM_INPUTS] = {0};
 uint8_t io_out_ptr = 0;
 
 uint8_t io_read(uint32_t addr) {
     uint8_t val = io_inputs[io_in_ptr];
-    printf("Reading io addr = %d, val = %d\n", io_in_ptr, val);
+    printf("Reading io addr = %d (%d), val = %d\n", addr, io_in_ptr, val);
     io_in_ptr += 1;
     return val;
 }
 
 void io_write(uint32_t addr, uint8_t val) {
-    printf("Writing io addr = %d, val = %d (%d)\n", io_out_ptr, val, &io_out_ptr);
+    printf("Writing io addr = %d (%d), val = %d\n", io_out_ptr, addr, val);
     io_outputs[io_out_ptr] = val;
     io_out_ptr +=1;
 }
 
 int main(void) {
-    init(); // Init CPU
+    init();         // Init CPU
     set_mem_read_func(mem_read);
     set_mem_write_func(mem_write);
     set_io_read_func(io_read);
@@ -72,8 +72,13 @@ int main(void) {
         printf("Error: counter overflow");
         return EXIT_FAILURE;
     }
-    for(uint32_t i=0; i<sizeof(io_inputs); i++) {
-        printf("i = %d, input = %d, output = %d\n", i, io_inputs[i], io_outputs[i]);
+    for(uint32_t i=0; i<NUM_INPUTS; i++) {
+        if(io_outputs[i] != (4 + io_inputs[i])) {
+            printf("Error: output value is not correct!\n");
+            printf("i = %d, input = %d, output = %d\n", i, io_inputs[i], io_outputs[i]);
+            return EXIT_FAILURE;
+        }
     }
+    printf("Success! Completed in %d steps!\n");
     return EXIT_SUCCESS;
 }
