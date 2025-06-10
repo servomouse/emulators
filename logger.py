83d00e9d9e28e659a4f06a7ff4d7595cfde74082
servomouse <servomouse@protonmail.com>
2025-06-09 21:04:22 -0600

    Make logger print to console instead of file when needed

--- logger.py
+++ logger.py
@@ -3,12 +3,17 @@ import sys
 
 
 class Logger:
-    def __init__(self, ui_getter):
+    def __init__(self, ui_getter, print_to_file=False):
         self.ui_getter = ui_getter
+        self.print_to_file = print_to_file
         self.callback_type = ctypes.CFUNCTYPE(None, ctypes.c_char_p, ctypes.c_char_p)
 
     def print(self, filename, logstring):
-        print(logstring)
+        if self.print_to_file:
+            with open(filename, 'a') as f:
+                f.write(logstring + "\n")
+        else:
+            self.print_line(logstring)
 
     def print_line(self, line):
         part_cmd = self.ui_getter.get_part_input()
