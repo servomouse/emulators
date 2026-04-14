989afaf8c12ede379e326742b15367ee41fc3262
servomouse <servomouse@protonmail.com>
2026-04-13 23:21:07 -0600

    Commonize cpu class

--- cpp/simple_cpu/cpu.hpp
+++ cpp/simple_cpu/cpu.hpp
@@ -6,43 +6,14 @@
 #include "addr_decoder.hpp"
 #include "memory.hpp"
 
-struct Registers {
-    uint16_t PC; // Program Counter
-    uint16_t SP; // Stack Pointer
-    uint8_t  A, X, Y; // Accumulator and Index registers
-    uint8_t  Status;  // Flags
-};
-
 class CPU {
-private:
-    Registers reg;
+public:
     AddressDecoder* mem_bus;
     AddressDecoder* io_bus;
-
-public:
     CPU(AddressDecoder* mem, AddressDecoder* io) 
-        : mem_bus(mem), io_bus(io) {
-        reset();
-    }
+        : mem_bus(mem), io_bus(io) { }
 
-    void reset() {
-        reg.PC = 0x0000;
-        reg.SP = 0xFD;
-        reg.A = reg.X = reg.Y = 0;
-        reg.Status = 0x00;
-        std::cout << "CPU Reset performed.\n";
-    }
-
-    void tick() {
-        // Fetch-Decode-Execute logic will go here
-        // For now, just a placeholder increment
-        uint8_t opcode = mem_bus->read(reg.PC);
-        std::cout << "Ticking... Read Opcode: " << (int)opcode << " at " << reg.PC << "\n";
-        reg.PC++;
-    }
-
-    void interrupt() {
-        // Push PC to stack, set interrupt flag, jump to vector
-        std::cout << "Interrupt triggered.\n";
-    }
+    virtual void reset()  = 0;
+    virtual void tick()  = 0;
+    virtual void interrupt() = 0;
 };
--- cpp/simple_cpu/main.cpp
+++ cpp/simple_cpu/main.cpp
@@ -5,22 +5,54 @@
 #include "memory.hpp"
 #include "cpu.hpp"
 
+struct Registers {
+    uint16_t PC; // Program Counter
+    uint16_t SP; // Stack Pointer
+    uint8_t  A, X, Y; // Accumulator and Index registers
+    uint8_t  Status;  // Flags
+};
+
+class MyCPU : public CPU {
+    Registers reg;
+public:
+    MyCPU(AddressDecoder* mem, AddressDecoder* io) 
+        : CPU(mem, io) {
+        reset();
+    }
+    void reset() override {
+        reg.PC = 0x0000;
+        reg.SP = 0xFD;
+        reg.A = reg.X = reg.Y = 0;
+        reg.Status = 0x00;
+        std::cout << "CPU Reset performed.\n";
+    }
+
+    void tick() override {
+        // Fetch-Decode-Execute logic will go here
+        // For now, just a placeholder increment
+        uint8_t opcode = mem_bus->read(reg.PC);
+        std::cout << "Ticking... Read Opcode: " << (int)opcode << " at " << reg.PC << "\n";
+        reg.PC++;
+    }
+
+    void interrupt() override {
+        // Push PC to stack, set interrupt flag, jump to vector
+        std::cout << "Interrupt triggered.\n";
+    }
+};
+
 int main() {
-    // 1. Create Hardware
-    RAM main_ram(0x8000);   // 32KB RAM
-    RAM bios_rom(0x8000);   // 32KB ROM (using RAM class for simplicity)
+    Memory ram(0x8000);   // 32KB RAM
+    Memory rom(0x8000);   // 32KB ROM (using RAM class for simplicity)
     
     AddressDecoder mem_decoder;
     AddressDecoder io_decoder;
 
-    // 2. Map Memory: RAM at 0x0000, ROM at 0x8000
-    mem_decoder.map_device(0x0000, 0x7FFF, &main_ram);
-    mem_decoder.map_device(0x8000, 0xFFFF, &bios_rom);
+    mem_decoder.map_device(0x0000, 0x7FFF, &ram);
+    mem_decoder.map_device(0x8000, 0xFFFF, &rom);
 
-    // 3. Connect CPU
-    CPU my_cpu(&mem_decoder, &io_decoder);
+    MyCPU my_cpu(&mem_decoder, &io_decoder);
 
-    // Test a tick
     my_cpu.tick();
 
     return 0;
