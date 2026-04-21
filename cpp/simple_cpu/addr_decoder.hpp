2d8885b8aec88b881f3e399ebb509685c5b9b2e7
servomouse <servomouse@protonmail.com>
2026-04-20 22:13:39 -0600

    Add logs to address decoder

--- cpp/simple_cpu/addr_decoder.hpp
+++ cpp/simple_cpu/addr_decoder.hpp
@@ -14,27 +14,47 @@ struct Mapping {
 class AddressDecoder : public BusDevice {
 private:
     std::vector<Mapping> regions;
+    bool print_log;
+    const char *space_name;
+    uint16_t unmapped_read_value = 0xFF;
 
 public:
     void map_device(uint16_t start, uint16_t end, BusDevice* device) {
         regions.push_back({start, end, device});
     }
-
     uint8_t read(uint16_t address) override {
         for (auto& region : regions) {
             if (address >= region.start && address <= region.end) {
-                return region.device->read(address - region.start);
+                uint16_t val = region.device->read(address - region.start);
+                if (print_log) {
+                    printf("%s: Read value 0x%X from 0x%X\n", space_name? space_name: "Mem_space", val, address);
+                }
+                return val;
             }
         }
-        return 0xFF; // Open bus behavior
+        if (print_log) {
+            printf("%s: Unmapped read from 0x%X, returning 0x%X\n", space_name? space_name: "Mem_space", address, unmapped_read_value);
+        }
+        return unmapped_read_value;
     }
-
     void write(uint16_t address, uint8_t data) override {
+        if (print_log) {
+            printf("%s: Writing 0x%X to 0x%X\n", space_name? space_name: "Mem_space", data, address);
+        }
         for (auto& region : regions) {
             if (address >= region.start && address <= region.end) {
                 region.device->write(address - region.start, data);
                 return;
             }
         }
+    }
+    void enable_log(void) {
+        print_log = true;
+    }
+    void disable_log(void) {
+        print_log = false;
+    }
+    void set_memspace_name(const char *ms_name) {
+        space_name = ms_name;
     }
 };
