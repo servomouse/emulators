782f529032c517fa04ace5aacc7b04d74a42144c
servomouse <servomouse@protonmail.com>
2026-04-24 23:02:45 -0600

    Add source code for the print procedure

---
+++ cpp/simple_cpu/zx_spectrum/cp_m_print_routine.asm
@@ -0,0 +1,38 @@
+; --- Address 0xF000: CP/M BDOS Entry Point ---
+ORG F000h
+
+BDOS_ENTRY:
+    PUSH AF             ; Save registers to avoid corrupting test state
+    PUSH BC
+    PUSH DE
+    PUSH HL
+
+    LD A, C             ; Load function code into A
+    CP 02h              ; Function 2: Character output?
+    JR Z, PRINT_CHAR
+    CP 09h              ; Function 9: String output?
+    JR Z, PRINT_STRING
+    JR EXIT_BDOS        ; Otherwise, just return
+
+PRINT_CHAR:
+    LD A, E             ; The character to print is in E
+    OUT (01h), A        ; Output to port 0x01 (or whichever port you prefer)
+    JR EXIT_BDOS
+
+PRINT_STRING:
+    ; DE points to the start of the string
+    EX DE, HL           ; Move string pointer to HL for easier access
+STRING_LOOP:
+    LD A, (HL)          ; Load character
+    CP '$'              ; Check for CP/M string terminator
+    JR Z, EXIT_BDOS
+    OUT (01h), A        ; Output character to the host
+    INC HL              ; Next character
+    JR STRING_LOOP
+
+EXIT_BDOS:
+    POP HL              ; Restore registers
+    POP DE
+    POP BC
+    POP AF
+    RET                 ; Return to the calling program (ZEXALL)
