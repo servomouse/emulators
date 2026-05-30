c4717345b059724703fad66f0f8fc3b4ef23a860
servomouse <servomouse@protonmail.com>
2026-05-29 22:00:20 -0600

    Update ALU

--- cpp/simple_cpu/4-bit_ALU.hpp
+++ cpp/simple_cpu/4-bit_ALU.hpp
@@ -47,185 +47,90 @@ private:
         return ((s1 == s2) && (sr != s1));
     }
 
-    // --- ALU operations ---
-    void op_add(void) {
-        // Does ADD and ADC: set Carry prior the execution if needed
+    void alu_execute_operation(ALU_Op operation) {
         uint8_t num_nibbles = _num_bits >> 2;
         uint8_t last_nibble = num_nibbles - 1;
-        uint16_t final_res = 0;
-        uint16_t carry = flags[get_flag_index(ALU_Flags::C)]? 1: 0;
-        uint16_t half_carry = 0;
-        uint16_t res = 0;
         uint16_t sb1 = (_op1 >> (_num_bits-1)) & 1;
         uint16_t sb2 = (_op2 >> (_num_bits-1)) & 1;
+        uint16_t carry = flags[get_flag_index(ALU_Flags::C)]? 1: 0;
+        uint16_t half_carry = 0;
+        uint16_t final_res = 0;
+        uint16_t res = 0;
+
+        // Parity calculation helper:
+        //Nibble value:              0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F
+        uint8_t nibble_parity[16] = {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0};
+        uint8_t parity_flag = 0;
 
         for (int i = 0; i < num_nibbles; i++) {
             uint16_t nibble_offset = i * 4;
             uint16_t n1 = (_op1 >> nibble_offset) & 0x0F;
             uint16_t n2 = (_op2 >> nibble_offset) & 0x0F;
+            uint16_t temp_res;
 
-            uint16_t temp_res = n1 + n2 + carry;
-            carry = (temp_res > 0x0F) ? 1 : 0;
+            switch (operation) {
+                case ALU_Op::ADD:
+                    temp_res = n1 + n2 + carry;
+                    carry = (temp_res > 0x0F) ? 1 : 0;
+                    break;
+                case ALU_Op::SUB:
+                    temp_res = n1 - (n2 + carry);
+                    carry = ((n2 + carry) > n1) ? 1 : 0;
+                    break;
+                case ALU_Op::AND:
+                    temp_res = n1 & n2;
+                    break;
+                case ALU_Op::XOR:
+                    temp_res = n1 ^ n2;
+                    break;
+                case ALU_Op::OR:
+                    temp_res = n1 | n2;
+                    break;
+                default:
+                    printf("Error: Unknown ALU operation: 0x%X\n", static_cast<size_t>(operation));
+                    exit(EXIT_FAILURE);
+            }
             temp_res &= 0x0F;
 
             res |= temp_res << nibble_offset;
-
+            parity_flag = (parity_flag + nibble_parity[temp_res]) & 1;
+            if (i < last_nibble) {
+                half_carry = carry;
+            }
             if (i < last_nibble) {
                 half_carry = carry;
             }
         }
         _res = res;
+
         // Update flags
-        bool zero_flag = res == 0;
-        
-        int parity_flag = 0;
-        for (int i=0; i<16; i++) {
-            parity_flag ^= (res >> i) & 1;
-        }
         uint16_t sbr = (res >> (_num_bits-1)) & 1;
 
         flags[get_flag_index(ALU_Flags::C)] = carry == 1;
         flags[get_flag_index(ALU_Flags::H)] = half_carry == 1;
         flags[get_flag_index(ALU_Flags::S)] = sbr;
-        flags[get_flag_index(ALU_Flags::Z)] = zero_flag;
+        flags[get_flag_index(ALU_Flags::Z)] = res == 0;
         flags[get_flag_index(ALU_Flags::P)] = parity_flag == 0; // 0 if even, 1 if odd
-        flags[get_flag_index(ALU_Flags::O)] = get_signed_overflow_flag(sb1, sb2, sbr, false);
+        flags[get_flag_index(ALU_Flags::O)] = get_signed_overflow_flag(sb1, sb2, sbr, operation==ALU_Op::SUB);
+    }
+
+    // --- ALU operations ---
+    void op_add(void) {
+        // Does ADD and ADC: set Carry prior the execution if needed
+        alu_execute_operation(ALU_Op::ADD);
     }
     void op_sub(void) {
         // Does SUB and SBC: set Carry prior the execution if needed
-        uint8_t num_nibbles = _num_bits >> 2;
-        uint8_t last_nibble = num_nibbles - 1;
-        uint16_t final_res = 0;
-        uint16_t borrow = flags[get_flag_index(ALU_Flags::C)]? 1: 0;
-        uint16_t half_borrow = 0;
-        uint16_t res = 0;
-        uint16_t sb1 = (_op1 >> (_num_bits-1)) & 1;
-        uint16_t sb2 = (_op2 >> (_num_bits-1)) & 1;
-
-        for (int i = 0; i < num_nibbles; i++) {
-            uint16_t nibble_offset = i * 4;
-            uint16_t n1 = (_op1 >> nibble_offset) & 0x0F;
-            uint16_t n2 = (_op2 >> nibble_offset) & 0x0F;
-
-            uint16_t temp_res = n1 - (n2 + borrow);
-            borrow = ((n2 + borrow) > n1) ? 1 : 0;
-            temp_res &= 0x0F;
-
-            res |= temp_res << nibble_offset;
-
-            if (i < last_nibble) {
-                half_borrow = borrow;
-            }
-        }
-        _res = res;
-        // Update flags
-        bool zero_flag = res == 0;
-        
-        int parity_flag = 0;
-        for (int i=0; i<16; i++) {
-            parity_flag ^= (res >> i) & 1;
-        }
-        uint16_t sbr = (res >> (_num_bits-1)) & 1;
-
-        flags[get_flag_index(ALU_Flags::C)] = borrow == 1;
-        flags[get_flag_index(ALU_Flags::H)] = half_borrow == 1;
-        flags[get_flag_index(ALU_Flags::S)] = sbr;
-        flags[get_flag_index(ALU_Flags::Z)] = zero_flag;
-        flags[get_flag_index(ALU_Flags::P)] = parity_flag == 0; // 0 if even, 1 if odd
-        flags[get_flag_index(ALU_Flags::O)] = get_signed_overflow_flag(sb1, sb2, sbr, true);
+        alu_execute_operation(ALU_Op::SUB);
     }
     void op_and(void) {
-        // Does AND
-        uint8_t num_nibbles = _num_bits >> 2;
-        uint8_t last_nibble = num_nibbles - 1;
-        uint16_t final_res = 0;
-        uint16_t res = 0;
-
-        for (int i = 0; i < num_nibbles; i++) {
-            uint16_t nibble_offset = i * 4;
-            uint16_t n1 = (_op1 >> nibble_offset) & 0x0F;
-            uint16_t n2 = (_op2 >> nibble_offset) & 0x0F;
-
-            uint16_t temp_res = n1 & n2;
-            temp_res &= 0x0F;
-
-            res |= temp_res << nibble_offset;
-        }
-        _res = res;
-        // Update flags
-        bool zero_flag = res == 0;
-        
-        int parity_flag = 0;
-        for (int i=0; i<16; i++) {
-            parity_flag ^= (res >> i) & 1;
-        }
-        uint16_t sbr = (res >> (_num_bits-1)) & 1;
-
-        flags[get_flag_index(ALU_Flags::S)] = sbr;
-        flags[get_flag_index(ALU_Flags::Z)] = zero_flag;
-        flags[get_flag_index(ALU_Flags::P)] = parity_flag == 0; // 0 if even, 1 if odd
+        alu_execute_operation(ALU_Op::AND);
     }
     void op_xor(void) {
-        // Does XOR
-        uint8_t num_nibbles = _num_bits >> 2;
-        uint8_t last_nibble = num_nibbles - 1;
-        uint16_t final_res = 0;
-        uint16_t res = 0;
-
-        for (int i = 0; i < num_nibbles; i++) {
-            uint16_t nibble_offset = i * 4;
-            uint16_t n1 = (_op1 >> nibble_offset) & 0x0F;
-            uint16_t n2 = (_op2 >> nibble_offset) & 0x0F;
-
-            uint16_t temp_res = n1 ^ n2;
-            temp_res &= 0x0F;
-
-            res |= temp_res << nibble_offset;
-        }
-        _res = res;
-        // Update flags
-        bool zero_flag = res == 0;
-        
-        int parity_flag = 0;
-        for (int i=0; i<16; i++) {
-            parity_flag ^= (res >> i) & 1;
-        }
-        uint16_t sbr = (res >> (_num_bits-1)) & 1;
-
-        flags[get_flag_index(ALU_Flags::S)] = sbr;
-        flags[get_flag_index(ALU_Flags::Z)] = zero_flag;
-        flags[get_flag_index(ALU_Flags::P)] = parity_flag == 0; // 0 if even, 1 if odd
+        alu_execute_operation(ALU_Op::XOR);
     }
     void op_or(void) {
-        // Does OR
-        uint8_t num_nibbles = _num_bits >> 2;
-        uint8_t last_nibble = num_nibbles - 1;
-        uint16_t final_res = 0;
-        uint16_t res = 0;
-
-        for (int i = 0; i < num_nibbles; i++) {
-            uint16_t nibble_offset = i * 4;
-            uint16_t n1 = (_op1 >> nibble_offset) & 0x0F;
-            uint16_t n2 = (_op2 >> nibble_offset) & 0x0F;
-
-            uint16_t temp_res = n1 | n2;
-            temp_res &= 0x0F;
-
-            res |= temp_res << nibble_offset;
-        }
-        _res = res;
-        // Update flags
-        bool zero_flag = res == 0;
-        
-        int parity_flag = 0;
-        for (int i=0; i<16; i++) {
-            parity_flag ^= (res >> i) & 1;
-        }
-        uint16_t sbr = (res >> (_num_bits-1)) & 1;
-
-        flags[get_flag_index(ALU_Flags::S)] = sbr;
-        flags[get_flag_index(ALU_Flags::Z)] = zero_flag;
-        flags[get_flag_index(ALU_Flags::P)] = parity_flag == 0; // 0 if even, 1 if odd
+        alu_execute_operation(ALU_Op::OR);
     }
     // --- !ALU operations ---
 
